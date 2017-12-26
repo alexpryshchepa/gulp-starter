8b7d8108a2f39e78ebc5d676e139922adb6e87d5
alex <alexandr.pryshchepa@gmail.com>
2017-12-26 17:38:22 +0200

    Fix lintes tasks

--- gulpfile.babel.js/tasks/sasslint/index.js
+++ gulpfile.babel.js/tasks/sasslint/index.js
@@ -7,18 +7,7 @@ import {
 
 export default gulp.task('sasslint', () => {
   return gulp.src(path.join(paths.src, '/**/*.s+(a|c)ss'))
-    .pipe(sassLint({
-      rules: {
-        'no-ids': 1,
-        'no-important': 1,
-        'indentation': 2,
-        'no-vendor-prefixes': 0,
-        'no-color-literals': 1,
-        'no-extends': 1,
-        'no-duplicate-properties': 1,
-        'no-transition-all': 1,
-      }
-    }))
+    .pipe(sassLint())
     .pipe(sassLint.format())
     .pipe(sassLint.failOnError())
 });
