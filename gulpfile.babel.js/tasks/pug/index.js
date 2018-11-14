69795ba411fda18aa3e10c5abc610e4531212843
Alex <alexandr.pryshchepa@gmail.com>
2018-11-15 00:53:04 +0200

    Update pug task to allow multiple entries

--- gulpfile.babel.js/tasks/pug/index.js
+++ gulpfile.babel.js/tasks/pug/index.js
@@ -6,16 +6,27 @@ import notify from 'gulp-notify';
 import { paths } from '../../config';
 
 export default gulp.task('pug', () => {
-  return gulp.src(path.resolve(paths.src, paths.pug.src))
-    .pipe(plumber({
-      errorHandler: function (err) {
-        notify.onError({
-          title: "Gulp error in " + err.plugin,
-          message: err.toString()
-        })(err);
-        this.emit('end');
-      }
-    }))
-    .pipe(pug())
-    .pipe(gulp.dest(path.resolve(paths.dist, paths.pug.dist)));
+  const task = function (src) {
+    return gulp.src(src)
+      .pipe(plumber({
+        errorHandler: function (err) {
+          notify.onError({
+            title: "Gulp error in " + err.plugin,
+            message: err.toString()
+          })(err);
+          this.emit('end');
+        }
+      }))
+      .pipe(pug())
+      .pipe(gulp.dest(path.resolve(paths.dist, paths.pug.dist)));
+  };
+
+  if (Array.isArray(paths.pug.src)) {
+    const entries = paths.pug.src.map(path => `${paths.src}/${path}`);
+    const tasks = entries.map(entry => task(entry));
+
+    return es.merge(tasks).on('end', done);
+  }
+
+  return task(path.resolve(paths.src, paths.pug.src));
 });
