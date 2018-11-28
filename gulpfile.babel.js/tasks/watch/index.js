ebeaf18951df6255735f7e35c4d6f94603a68f97
alex <alexandr.pryshchepa@gmail.com>
2018-11-28 11:31:38 +0200

    Add html files watching

--- gulpfile.babel.js/tasks/watch/index.js
+++ gulpfile.babel.js/tasks/watch/index.js
@@ -4,7 +4,7 @@ import { paths } from '../../config';
 
 export default gulp.task('watch', () => {
 //  gulp.watch(`${paths.src}/**/*.pug`, gulp.series('pug')).on('change', browserSync.reload);
-  gulp.watch(`${paths.src}/**/*.ejs`, gulp.series('ejs')).on('change', browserSync.reload);
+  gulp.watch([`${paths.src}/**/*.ejs`, `${paths.src}/**/*.html`], gulp.series('ejs')).on('change', browserSync.reload);
   gulp.watch(`${paths.src}/**/*.scss`, gulp.series('sass')).on('change', browserSync.reload);
   gulp.watch(`${paths.src}/**/*.js`, gulp.series('scripts')).on('change', browserSync.reload);
   gulp.watch(`${paths.src}/images/**/*`, gulp.series('images')).on('change', browserSync.reload);
